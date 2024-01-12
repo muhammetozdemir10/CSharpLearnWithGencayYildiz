23f3ffa2174bff76cb71c863058aef734ba6f120
muhammetozdemir10 <127602475+muhammetozdemir10@users.noreply.github.com>
2024-01-12 19:28:53 +0300

    Create LoginApp.cs

---
+++ Variables/Applications/LoginApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topics.Applications
+{
+    internal class LoginApp
+    {
+        static void Start()
+        {
+            Console.WriteLine("Kullanici adini giriniz");
+            string? userName = Console.ReadLine();
+
+            Console.WriteLine("Sifrenizi giriniz");
+            string? userPassword = Console.ReadLine();
+            if (userName == "Wizee" && userPassword == "123")
+            {
+                Console.WriteLine("Giris basarili");
+            }
+            else
+            {
+                Console.WriteLine("Girdiginiz kullani adi veya sifre yanlis ");
+            }
+        }
+    }
+}
