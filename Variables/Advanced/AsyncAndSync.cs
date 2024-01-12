ec1e0d7b67f95e63404581d3875871ee3ecf9457
muhammetozdemir10 <127602475+muhammetozdemir10@users.noreply.github.com>
2024-01-12 19:28:49 +0300

    Create AsyncAndSync.cs

---
+++ Variables/Advanced/AsyncAndSync.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topics.Advanced
+{
+    internal class AsyncAndSync
+    {
+
+
+        static void AsyncFunction()
+        {
+            var t = new Task(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"Current index: {i}");
+                    Thread.Sleep(1000);
+                }
+            });
+            t.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Aslan: {i}");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Merhaba ben kodun son mesajiyim.");
+        }
+
+        static void SyncFunction()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Current index: {i}");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Merhaba ben kodun son mesajiyim.");
+        }
+    }
+}
